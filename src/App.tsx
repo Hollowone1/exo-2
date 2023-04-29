5929f7ae2ac4ee9cd0d10e528c2178048d015ec2
Antoine Stoecklin <astoeck@hotmail.fr>
2023-04-29 18:22:34 +0200

    early exercice done

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import React from 'react';
 import POKEMONS from "./models/mock-pockemon";
 import Pokemon from "./models/pokemon";
 
   
 const App: React.FC = () => {
-const [pokemons] = useState<Pokemon[]>(POKEMONS);
+const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+useEffect(() => {
+    setPokemons(POKEMONS);
+}, []);
 
  return (
     <div>
-       <h1>Pokédex</h1>
-       <p>il y a {pokemons.length} dans le pokédex</p>
+       <h1 className="center">Pokédex</h1>
+       <div className="container">
+        <div className="row">
+            {pokemons.map(({id,name,picture, created})=> {
+                <div className="col s6 m4" key={id}>
+                    <div className="card horizontal">
+                        <div className="card-image">
+                            <img src={picture} alt={name}></img>
+                        </div>
+                        <div className="card-stacked">
+                            <div className="card-content">
+                                <p>{name}</p>
+                                <p><small>{created.toString()}</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            })}
+        </div>
+       </div>
     </div>
-  
-  
  )
 }
   
