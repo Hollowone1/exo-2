54feb10d56c9d07b2c79e8f424ed1158dce141e8
Antoine Stoecklin <astoeck@hotmail.fr>
2023-05-02 11:26:51 +0200

    nav system done

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PokemonList from "./pages/pokemon-list";
 import { Link, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import PokemonsDetail from './pages/pokemon-detail';
-
+import PageNotFound from './pages/page-not-found';
   
 const App: React.FC = () => {
 
@@ -20,6 +20,7 @@ const App: React.FC = () => {
             <Route exact path="/" component={PokemonList}></Route>
             <Route exact path="/pokemons" component={PokemonList}></Route>
             <Route path="/pokemons/:id" component={PokemonsDetail}></Route>
+            <Route component={PageNotFound}></Route>
         </Switch>
         </div>
     </Router>
--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -3,6 +3,7 @@ import Pokemon from '../models/pokemon';
 import './pokemon-card.css'; 
 import formatDate from '../helpers/format-data';
 import formatType from '../helpers/format-type';
+import { useHistory } from 'react-router-dom';
   
 type Props = {
   pokemon: Pokemon,
@@ -12,6 +13,7 @@ type Props = {
 const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = '1px,solid,#192654'}) => {
     
     const [color, setColor] = useState<string>();
+    const history = useHistory();
 
     const showBorder = () => {
         setColor(borderColor);
@@ -21,8 +23,12 @@ const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = '1px,soli
         setColor('#f5f5f5');
     }
 
+    const goToPokemon = (id: number)=> {
+        history.push(`/pokemons/${id}`);
+    }
+
   return (
-    <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
+    <div className="col s6 m4" onClick={() => goToPokemon(pokemon.id)} onMouseEnter={showBorder} onMouseLeave={hideBorder}>
       <div className="card horizontal" style={{borderColor: color}}>
         <div className="card-image"> 
           <img src={pokemon.picture} alt={pokemon.name}/>
